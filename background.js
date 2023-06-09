90ed7afcc9001b1f3832a217c087e708871f4f86
Tiago-0liveira <tfgoliveira@gmail.com>
2023-06-10 00:29:19 +0100

    change so it doesnt error in console

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if (changeInfo.status == 'complete') {
+	if (changeInfo.status == 'complete' && tab.url.startsWith("https://chat.openai.com")) {
 		chrome.scripting.insertCSS({
 			target: {tabId},
 			files: ["chatGPT.css"],
